5c791c028a767b7899034f502b381fdcaec5ce3e
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-05 10:54:24 -0800

    Add/pass integ test for word select and view route

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -4,11 +4,19 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('create a word path', {:type => :feature}) do
-  it('creates a word and then goes to the word page') do
+  it('creates a word and then goes to the words page') do
     visit('/words')
     click_on('Add a new word')
     fill_in('word_name', :with => 'Apple')
     click_on('Add!')
     expect(page).to have_content('Apple')
+  end
+end
+
+describe('create a word path', {:type => :feature}) do
+  it('selects a word and goes to its dynamic word page') do
+    visit('/words')
+    click_on('Apple')
+    expect(page).to have_content('Edit name')
   end
 end
