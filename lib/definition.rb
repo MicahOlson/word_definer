3820a57dcde65d5f7a0929e96b59e0072d584bec
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-05 11:37:47 -0800

    Add class variable and code in all() method to pass spec

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,10 +1,13 @@
 class Definition
   attr_reader :meaning
 
+  @@definitions = {}
+
   def initialize(attrs)
     @meaning = attrs[:meaning]
   end
 
   def self.all
+    @@definitions.values
   end
 end
