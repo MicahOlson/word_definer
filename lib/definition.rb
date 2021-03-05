cc4e77afa267c5f5abb5f586f5d71543e4ade266
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-05 11:36:30 -0800

    Add red-stage spec for all() class method

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,7 +1,10 @@
 class Definition
   attr_reader :meaning
-  
+
   def initialize(attrs)
     @meaning = attrs[:meaning]
+  end
+
+  def self.all
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -10,4 +10,10 @@ describe '#Definition' do
       expect(apple.meaning).to(eq("a round fruit often red in color"))
     end
   end
+
+  describe('.all') do
+    it("returns an array of all saved definition objects (empty at first)") do
+      expect(Definition.all).to(eq([]))
+    end
+  end
 end
