a75aadfc495e61cbbd2d39903ef70e29b5506f15
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-05 11:05:55 -0800

    Add and test word update functionality

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -13,10 +13,21 @@ describe('create a word path', {:type => :feature}) do
   end
 end
 
-describe('create a word path', {:type => :feature}) do
+describe('select a word path', {:type => :feature}) do
   it('selects a word and goes to its dynamic word page') do
     visit('/words')
     click_on('Apple')
     expect(page).to have_content('Edit name')
+  end
+end
+
+describe('update a word path', {:type => :feature}) do
+  it('updates a word and then goes to the words page') do
+    visit('/words')
+    click_on('Apple')
+    click_on('Edit name')
+    fill_in('name', :with => 'Orange')
+    click_on('Update')
+    expect(page).to have_content('Orange')
   end
 end
