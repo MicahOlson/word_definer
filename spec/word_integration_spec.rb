71e75ea7c8c7341f145cb659233ed97a25d5c343
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-06 09:54:34 -0800

    Add integration spec for creating a new definition

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -7,16 +7,16 @@ describe('create a word path', {:type => :feature}) do
   it('creates a word and then goes to the words page') do
     visit('/words')
     click_on('Add a new word')
-    fill_in('word_name', :with => 'Kittens')
+    fill_in('word_name', :with => 'Apple')
     click_on('Add!')
-    expect(page).to have_content('Kittens')
+    expect(page).to have_content('Apple')
   end
 end
 
 describe('select a word path', {:type => :feature}) do
   it('selects a word and goes to its dynamic word page') do
     visit('/words')
-    click_on('Kittens')
+    click_on('Apple')
     expect(page).to have_content('Edit name')
   end
 end
@@ -24,18 +24,28 @@ end
 describe('update a word path', {:type => :feature}) do
   it('updates a word and then goes to the words page') do
     visit('/words')
-    click_on('Kittens')
+    click_on('Apple')
     click_on('Edit name')
-    fill_in('name', :with => 'Puppies')
+    fill_in('name', :with => 'Baby')
     click_on('Update')
-    expect(page).to have_content('Puppies')
+    expect(page).to have_content('Baby')
+  end
+end
+
+describe('create a definition path', {:type => :feature}) do
+  it("creates a definition and then goes to a word's definitions page") do
+    visit('/words')
+    click_on('Baby')
+    fill_in('meaning', :with => 'a very young or newly born child')
+    click_on('Add definition')
+    expect(page).to have_content('a very young or newly born child')
   end
 end
 
 describe('delete a word path', {:type => :feature}) do
   it('deletes a word and then goes to the words page') do
     visit('/words')
-    click_on('Puppies')
+    click_on('Baby')
     click_on('Edit name')
     click_on('Delete word')
     expect(page).to have_content('currently no words')
